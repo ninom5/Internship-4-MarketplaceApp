b4ac256c792e7684cd6fce40f4f9a9e6c4f669a1
ninom5 <nmilas00@fesb.hr>
2024-11-23 15:52:42 +0100

    intial commit

---
+++ Internship-4-OOP2/Internship-4-OOP2/Data/Enum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship_4_OOP2.Data
+{
+    internal interface Enum
+    {
+    }
+}
