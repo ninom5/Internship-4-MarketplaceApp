c0f460b32f5168cb9d3220eafd8aca1bc83f4eec
ninom5 <nmilas00@fesb.hr>
2024-11-25 18:35:05 +0100

    created 3 layers; inside data layer added enums and basic classes

---
+++ MarketplaceApp/MarketplaceApp/Models/Seller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketplaceApp.Data.Models
+{
+    public class Seller : User
+    {
+        public double Earnings { get; set; }
+        public Seller(string name, string email) : base(name, email)
+        {
+            Earnings = 0;
+        }
+    }
+}
